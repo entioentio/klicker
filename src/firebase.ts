5a9ba0ab9b9c6f76123cbceaf3024271d7c76ebf
Przemek Maszczynski <gh@entio.me>
2023-02-28 08:29:59 +0100

    Changed config

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,19 @@ import { getFirestore } from "firebase/firestore";
 // ... other firebase imports
 
 export const firebaseApp = initializeApp({
-  apiKey: "AIzaSyCzO1tFIPeqvvUhb_wz7xydzjXQIM6Ck2o",
+  /* apiKey: "AIzaSyCzO1tFIPeqvvUhb_wz7xydzjXQIM6Ck2o",
   authDomain: "kicker-app-9cd86.firebaseapp.com",
   projectId: "kicker-app-9cd86",
   storageBucket: "kicker-app-9cd86.appspot.com",
   messagingSenderId: "269451121959",
   appId: "1:269451121959:web:cc44393a0239a532bd4502",
-  measurementId: "G-ELFGF2JKB9",
+  measurementId: "G-ELFGF2JKB9", */
+  apiKey: "AIzaSyCejCxyBSOJ7WMboCu8GDkkPyXzbUASuew",
+    authDomain: "kicker-olama.firebaseapp.com",
+    projectId: "kicker-olama",
+    storageBucket: "kicker-olama.appspot.com",
+    messagingSenderId: "489009171392",
+    appId: "1:489009171392:web:72936f3d219a77d2701625",
 });
 
 // used for the firestore refs
