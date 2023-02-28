03d3de100774a742b844ebd02c5fa4ed1760a97f
Przemek Maszczynski <gh@entio.me>
2023-02-28 08:31:05 +0100

    Lol

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,6 @@ import { getFirestore } from "firebase/firestore";
 // ... other firebase imports
 
 export const firebaseApp = initializeApp({
-  /* apiKey: "AIzaSyCzO1tFIPeqvvUhb_wz7xydzjXQIM6Ck2o",
-  authDomain: "kicker-app-9cd86.firebaseapp.com",
-  projectId: "kicker-app-9cd86",
-  storageBucket: "kicker-app-9cd86.appspot.com",
-  messagingSenderId: "269451121959",
-  appId: "1:269451121959:web:cc44393a0239a532bd4502",
-  measurementId: "G-ELFGF2JKB9", */
   apiKey: "AIzaSyCejCxyBSOJ7WMboCu8GDkkPyXzbUASuew",
     authDomain: "kicker-olama.firebaseapp.com",
     projectId: "kicker-olama",
